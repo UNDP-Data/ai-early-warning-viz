ae4577866d5968e4e9a50b898bec7c2668ceab50
Mustafa Saifee <mustafa.saifee@undp.org>
2023-03-08 16:35:46 +0200

    Create Language.tsx

---
+++ src/Language.tsx
@@ -0,0 +1,122 @@
+export const Translation = [
+    {
+        key:'Methodology',
+        en:'Methodology',
+        es:'Metodología',
+    },
+    {
+        key:'All',
+        en:'All',
+        es:'Todos',
+    },
+    {
+        key:'Men',
+        en:'Men',
+        es:'Hombres',
+    },
+    {
+        key:'Women',
+        en:'Women',
+        es:'Mujeres',
+    },
+    {
+        key:'Hate Speech',
+        en:'Hate Speech',
+        es:'Discurso del odio',
+    },
+    {
+        key:'Education',
+        en:'Education',
+        es:'Educación',
+    },
+    {
+        key:'Politics',
+        en:'Politics',
+        es:'Política',
+    },
+    {
+        key:'Reproduction',
+        en:'Reproduction',
+        es:'Reproducción',
+    },
+    {
+        key:'Violence',
+        en:'Violence',
+        es:'Violencia',
+    },
+    {
+        key:'Employment',
+        en:'Employment',
+        es:'Empleo',
+    },
+    {
+        key:'Tweet by gender',
+        en:'Tweet by gender',
+        es:'Tweets por género',
+    },
+    {
+        key:'Total tweets',
+        en:'Total tweets',
+        es:'Tweets totales',
+    },
+    {
+        key:'Tweets with hate speech',
+        en:'Tweets with hate speech',
+        es:'Tweets con discurso de odio',
+    },
+    {
+        key:'Tweets without hate speech',
+        en:'Tweets without hate speech',
+        es:'Tweets sin discurso de odio',
+    },
+    {
+        key:'Hate speech tweets',
+        en:'Hate speech tweets',
+        es:'Tweets de incitación al odio',
+    },
+    {
+        key:'Tweets by categories',
+        en:'Tweets by categories',
+        es:'Tweets por categorías',
+    },
+    {
+        key:'Tweet trends over time',
+        en:'Tweet trends over time',
+        es:'Tendencias de Tweets a lo largo del tiempo',
+    },
+    {
+        key:'All tweets',
+        en:'All tweets',
+        es:'Todos los Tweets',
+    },
+    {
+        key:'Day',
+        en:'Day',
+        es:'Diario',
+    },
+    {
+        key:'Hourly',
+        en:'Hourly',
+        es:'Horario',
+    },
+    {
+        key:'Tweets by men',
+        en:'Tweets by men',
+        es:'Tweets de Hombres',
+    },
+    {
+        key:'Tweets by women',
+        en:'Tweets by women',
+        es:'Tweets de Mujeres',
+    },
+    {
+        key:'Tweets with hate speech by men',
+        en:'Tweets with hate speech by men',
+        es:'Tweets con discurso de odio de hombres',
+    },
+    {
+        key:'Tweets with hate speech by women',
+        en:'Tweets with hate speech by women',
+        es:'Tweets con discurso de odio de mujeres',
+    },
+]
